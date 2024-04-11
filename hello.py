e4afe94d024de49336ad100a67dff21f4b3e5e59
paul-laird <35932444+paul-laird@users.noreply.github.com>
2024-04-11 20:49:55 +0100

    Update hello.py
    Stub response

--- hello.py
+++ hello.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello again from Dockerised Flask"
+    return '''{"Result":[{"Name":"Paul","Email":"test1@test.ie","ID":1},{"Name":"John","Email":"test2@test.ie","ID":2}],
+      "count":2}'''
 
 @app.route("/route32a")
 def route32a():
